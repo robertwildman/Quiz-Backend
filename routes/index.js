542c5e7e115037c99b525f3461dcfd9c50051307
Robert Wildman <robertwildman4@gmail.com>
2018-02-23 13:20:16 +0000

    Adding routers into system

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var usersrouter = require('./users.js');
+var questionsrouter = require('./questions.js');
+var categoriesrouter = require('./categories.js');
+var leaderboardrouter = require('./Leaderboard.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.send('Welcome to API');
 });
+
+router.use('/users', usersrouter);
+router.use('/questions', questionsrouter);
+router.use('/categories', categoriesrouter);
+router.use('/Leaderboard', leaderboardrouter);
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+    res.send('Welcome to User API');
 });
-
+router.get('/:id', function(req, res) {
+    res.send('User ID entered is '+ req.params.id);
+});
+router.get('/:id/getAnsweredQuestions', function(req, res) {
+    res.send('User ID entered is '+ req.params.id + ' Answer Questions');
+});
+router.get('/:id/getScores', function(req, res) {
+    res.send('User ID entered is '+ req.params.id + ' Get Scores ');
+});
+router.get('/:id/getCategories', function(req, res) {
+    res.send('User ID entered is f '+ req.params.id + ' Get Categories');
+});
 module.exports = router;
