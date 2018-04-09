61461fbaa167fd8dd023b4567fbe34adfd38e492
aaronamjid <38182559+aaronamjid@users.noreply.github.com>
2018-04-09 16:18:12 +0100

    admin views
    hub admin view, edit category view, edit question view, edit user view

--- views.js
+++ views.js
@@ -7,5 +7,16 @@ router.get('/', function(req, res) {
 router.get('/createaccount', function(req, res) {
     res.render('createaccount');
 });
-
+router.get('/admin', function(req, res) {
+    res.render('admin');
+});
+router.get('/edituser', function(req, res) {
+    res.render('edituser');
+});
+router.get('/editcategory', function(req, res) {
+    res.render('admin');
+});
+router.get('/editquestion', function(req, res) {
+    res.render('edituser');
+});
 module.exports = router;
