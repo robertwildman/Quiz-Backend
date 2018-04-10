c8e7478f6628af398784cd3ee6815a0eb92457e0
Robert Wildman <robertwildman4@gmail.com>
2018-04-10 10:23:32 +0100

    Admin Now displayed all users

--- views.js
+++ views.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./Config/db');
+var connection = db.connection;
 
 router.get('/', function(req, res) {
     res.render('login',{error:null});
@@ -8,15 +10,21 @@ router.get('/createaccount', function(req, res) {
     res.render('createaccount');
 });
 router.get('/admin', function(req, res) {
-    res.render('admin');
+  function results(results)
+  {
+    res.render('admin',{users:results});
+  }
+  //First bit is the query and the second is the callback needed to recive the results
+  db.query('SELECT * from user',results);
+
 });
 router.get('/edituser', function(req, res) {
     res.render('edituser');
 });
 router.get('/editcategory', function(req, res) {
-    res.render('admin');
+    res.render('editcategory');
 });
 router.get('/editquestion', function(req, res) {
-    res.render('edituser');
+    res.render('editquestion');
 });
 module.exports = router;
